cabf3c5517f3c5663701a3b181d254ea9a01c9e8
VitaliiVM <69699528+VitaliiVM@users.noreply.github.com>
2021-04-26 12:45:48 +0300

    Initialize project

--- src/App.js
+++ src/App.js
@@ -10,11 +10,18 @@ class App extends React.Component{
             count: 0,
             text : "",
             textwarning: "",
-            radio: ""
+            radio: "",
+            select: "",
+            width: "200px",
+            rangeValue: "50",
+            picture: "https://cdn3.iconfinder.com/data/icons/big-avatar-set/100/Avatar-39-256.png"
+
         }
         this.task1 = this.task1.bind(this);
         this.forCount = this.forCount.bind(this);
         this.ResetCount = this.ResetCount.bind(this);
+        this.divSize = this.divSize.bind(this);
+        this.hidepicture = this.hidepicture.bind(this);
     }
 
     task1() {
@@ -37,7 +44,7 @@ class App extends React.Component{
     inputChange = (e) => {
        this.setState({text: e.target.value});
        if (this.state.text.length < 6) {
-           this.setState({textwarning: "Длинна не меньше 6"});
+           this.setState({textwarning: "Длинна должна быть не меньше 6 символов"});
        } else {
            this.setState({textwarning: ""});
        }
@@ -56,6 +63,24 @@ class App extends React.Component{
      }
 
     }
+    inptValue = (e) => {
+        this.setState({select: e.target.value});
+        console.log(e.target.value);
+    }
+    divSize() {
+        this.setState({width:  this.state.width + 3});
+      console.log(this.state.width);
+    }
+    rangeValue = (e) => {
+        this.setState({rangeValue: e.target.value});
+    }
+    hidepicture()  {
+    if (this.state.picture) {
+        this.setState({picture: ""});
+    } else {
+        this.setState({picture: "https://cdn3.iconfinder.com/data/icons/big-avatar-set/100/Avatar-39-256.png"});
+    }
+    }
 
 
     render() {
@@ -63,6 +88,8 @@ class App extends React.Component{
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
+                    <img src={this.state.picture}/>
+                    <button onClick={this.hidepicture}>HidePicture</button>
                     <button onClick={this.task1}>Button</button>
                     <p>{this.state.work}</p>
                     <button onClick={this.forCount}>Count</button>
@@ -73,12 +100,15 @@ class App extends React.Component{
                         <button  className="atr1" data="atr-1"  onClick={this.showatr1}>atr1</button>
                         <button  className="atr2" data="atr-2" onClick={this.showatr1}>atr2</button>
                         <input type="text" onChange={this.inputChange}/>
-                        <select >
+                        <select onInput={this.inptValue}>
                             <option>Пункт 1</option>
                             <option>Пункт 2</option>
                             <option>Пункт 3</option>
                             <option>Пункт 4</option>
                         </select>
+                        <span>{this.state.select}</span>
+                        <input type="range" onChange={this.rangeValue}/>
+                        <span>{this.state.rangeValue}</span>
                     </div>
                     <p>{this.state.text}</p>
                     <span>{this.state.textwarning}</span>
@@ -91,6 +121,9 @@ class App extends React.Component{
                        <input type="radio" id="contactChoice1" name="contact" value="third" onInput={this.inpt2}/>
                        <span>{this.state.radio}</span>
                    </p>
+                    <div className="first" style={{"width": this.state.width}}></div>
+                    <button onClick={this.divSize}>DivSize</button>
+                    <button>DivReset</button>
 
                 </header>
             </div>
